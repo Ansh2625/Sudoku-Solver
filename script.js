58fcba6e69bb34e0bdb92f2657fd198632ef5ff5
Ansh Sharma <anshsharma2625@gmail.com>
2024-07-11 13:55:00 +0530

    Added OOPs Concepts

--- script.js
+++ script.js
@@ -1,137 +1,134 @@
-document.addEventListener("DOMContentLoaded",function()
+class SudokuSolver
 {
-    let board = document.getElementById("sudo-board");
-
-    for(let i=0; i<9; i++)
+    constructor(boardId)
     {
-        let row = document.createElement("tr");
-        for(let j=0; j<9; j++)
-        {
-            let cell = document.createElement("td");
-            let input = document.createElement("input");
-            input.setAttribute("type","number");
-            input.setAttribute("min","1");
-            input.setAttribute("max","9");
-            input.setAttribute("maxlength","1");
-            cell.appendChild(input);
-            row.appendChild(cell);
-        }
-        board.appendChild(row);
+        this.boardElement = document.getElementById(boardId);
+        this.createBoard();
     }
-});
 
-function getBoard()
-{
-    let board = [];
-    let rows = document.querySelectorAll("#sudo-board tr");
-    rows.forEach(row =>
+    createBoard()
     {
-        let rowData = [];
-        let cells = row.querySelectorAll("td input");
-        cells.forEach(cell =>
+        for(let i=0; i<9; i++)
         {
-            rowData.push(cell.value === "" ? 0 : parseInt(cell.value));
-        });
-        board.push(rowData);
-    });
-    return board;
-}
-
-function setBoard(board)
-{
-    let rows = document.querySelectorAll("#sudo-board tr");
-    rows.forEach((row,i) => 
-    {
-        let cells = row.querySelectorAll("td input");
-        cells.forEach((cell,j) =>
-        {
-            cell.value = board[i][j] === 0 ? "" : board[i][j];
-        });
-    });
-}
-
-function isSafe(board,row,col,num)
-{
-    for(let x=0; x<9; x++)
-    {
-        if(board[row][x]==num || board[x][col]==num || board[3*Math.floor(row/3) + Math.floor(x/3)][3*Math.floor(col/3) + x%3]==num)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-function solveSudoku(board)
-{
-    let row = -1;
-    let col = -1;
-    let isEmpty = true;
-
-    for(let i=0; i<9; i++)
-    {
-        for(let j=0; j<9; j++)
-        {
-            if(board[i][j] == 0)
+            let row = document.createElement("tr");
+            for(let j=0; j<9; j++)
             {
-                row = i;
-                col = j;
-                isEmpty = false;
-                break;
+                let cell = document.createElement("td");
+                let input = document.createElement("input");
+                input.setAttribute("type","number");
+                input.setAttribute("min","1");
+                input.setAttribute("max","9");
+                input.setAttribute("maxlength","1");
+                cell.appendChild(input);
+                row.appendChild(cell);
+            }
+            this.boardElement.appendChild(row);
+        }
+    }
+
+    getBoard()
+    {
+        let board = [];
+        let rows = this.boardElement.querySelectorAll("tr");
+        rows.forEach(row =>
+        {
+            let rowData = [];
+            let cells = row.querySelectorAll("td input");
+            cells.forEach(cell =>
+            {
+                rowData.push(cell.value === "" ? 0 : parseInt(cell.value));
+            });
+            board.push(rowData);
+        });
+        return board;
+    }
+
+    setBoard(board)
+    {
+        let rows = this.boardElement.querySelectorAll("tr");
+        rows.forEach((row,i) => 
+        {
+            let cells = row.querySelectorAll("td input");
+            cells.forEach((cell,j) =>
+            {
+                cell.value = board[i][j] === 0 ? "" : board[i][j];
+            });
+        });
+    }
+
+    isSafe(board,row,col,num)
+    {
+        for(let x=0; x<9; x++)
+        {
+            if(board[row][x]==num || board[x][col]==num || board[3*Math.floor(row/3) + Math.floor(x/3)][3*Math.floor(col/3) + x%3]==num)
+            {
+                return false;
             }
         }
-        if(!isEmpty)
-        {
-            break;
-        }
-    }
-
-    if(isEmpty)
-    {
         return true;
     }
 
-    for(let num=1; num<=9; num++)
+    solveSudoku(board)
     {
-        if(isSafe(board,row,col,num))
+        for(let i=0; i<9; i++)
         {
-            board[row][col] = num;
-            if(solveSudoku(board))
+            for(let j=0; j<9; j++)
             {
-                return true;
+                if(board[i][j] == 0)
+                {
+                    for(let k=1; k<=9; k++)
+                    {
+                        if(this.isSafe(board,i,j,k))
+                        {
+                            board[i][j] = k;
+                            if(this.solveSudoku(board))
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                board[row][col] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
             }
-            board[row][col] = 0;
+        }
+        return true;
+    }
+
+    generateUnsolvedBoard()
+    {
+        let unsolvedBoard = [
+            [0,7,5,0,9,0,0,0,6],
+            [0,2,3,0,8,0,0,4,0],
+            [8,0,0,0,0,3,0,0,1],
+            [5,0,0,7,0,2,0,0,0],
+            [0,4,0,8,0,6,0,2,0],
+            [0,0,0,9,0,1,0,0,3],
+            [9,0,0,4,0,0,0,0,7],
+            [0,6,0,0,7,0,5,8,0],
+            [7,0,0,0,1,0,3,9,0]
+        ];
+        this.setBoard(unsolvedBoard);
+    }
+
+    solveIt()
+    {
+        let board = this.getBoard();
+        if(this.solveSudoku(board))
+        {
+            this.setBoard(board);
+        }
+        else
+        {
+            alert("No solution exists");
         }
     }
-
-    return false;
 }
 
-function generateUnsolvedBoard()
+document.addEventListener("DOMContentLoaded",function()
 {
-    let unsolvedBoard = [
-        [0,7,5,0,9,0,0,0,6],
-        [0,2,3,0,8,0,0,4,0],
-        [8,0,0,0,0,3,0,0,1],
-        [5,0,0,7,0,2,0,0,0],
-        [0,4,0,8,0,6,0,2,0],
-        [0,0,0,9,0,1,0,0,3],
-        [9,0,0,4,0,0,0,0,7],
-        [0,6,0,0,7,0,5,8,0],
-        [7,0,0,0,1,0,3,9,0]
-    ];
-    setBoard(unsolvedBoard);
-}
-
-function solveIt()
-{
-    let board = getBoard();
-    if(solveSudoku(board))
-    {
-        setBoard(board);
-    }
-    else
-    {
-        alert("No solution exists");
-    }
-}
+    window.sudokuSolver = new SudokuSolver("sudo-board");
+});
